6b5c596526c8dd8879682070c45e3e012956add7
Federico Marini <marinif@uni-mainz.de>
2025-09-08 17:20:43 +0200

    R&man, function and imports handled

---
+++ R/iSEEWorkshopEuroBioc2025-pkg.R
@@ -0,0 +1,13 @@
+#' iSEEWorkshopEuroBioc2025: Learning to use the package iSEE
+#'
+#' A two-hours session to learn how to use in a reasonable depth the iSEE
+#' package
+#'
+#' @importFrom iSEE iSEE
+#' @importFrom SingleCellExperiment SingleCellExperiment
+#' @importFrom scater plotTSNE
+#'
+#' @name iSEEWorkshopEuroBioc2025-pkg
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
